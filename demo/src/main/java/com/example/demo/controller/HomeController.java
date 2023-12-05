141302a3a51070a92fa75b8e177c3972a3f222c9
ny_980321 <ny_980321@naver.com>
2023-12-05 14:35:11 +0900

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller; // com.example(backend)
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @RequestMapping("/mypage")
+    public String home(){
+        log.trace("trace!");
+        return "home";
+    }
+}
